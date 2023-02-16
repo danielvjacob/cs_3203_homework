33fa99c56b39e01109a7ac902194d41a1b31cd6b
danielvjacob <98068637+danielvjacob@users.noreply.github.com>
2023-02-15 21:48:16 -0600

    added product function

--- homework.java
+++ homework.java
@@ -2,11 +2,19 @@ package cs_3203_homework;
 
 public class homework {
 
-    
+
     public static int Sum(int[] array) {
         int result = 0;
         for (int i = 0; i < array.length; i++){
             result += array[i];
+        }
+        return result;
+    }
+
+    public static int Product(int[] array) {
+        int result = 1;
+        for (int i = 0; i < array.length; i++){
+            result = result * array[i];
         }
         return result;
     }
